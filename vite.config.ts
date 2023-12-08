93af231a639e380611d2342ecba3c985bb212a43
moelashmawy <ashmawy894@gmail.com>
2023-12-08 19:14:27 +0200

    Init

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslintPlugin from "vite-plugin-eslint";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    eslintPlugin({
+      cache: false,
+      include: ["./src/**/*.ts", "./src/**/*.tsx"],
+      exclude: [],
+    }),
+  ],
+  server: {
+    open: true,
+    port: 3000,
+  },
+});
